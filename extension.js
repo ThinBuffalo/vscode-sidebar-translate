897811292df8ea0fb6230a35569570cf06393c6d
Thin Buffalo <mcluozc@163.com>
2021-01-11 14:14:43 +0800

    2021/1/11

--- extension.js
+++ extension.js
@@ -18,13 +18,14 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('sidebar-translate.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('sbts.helloWorld', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from sidebar-translate!');
 	});
-
+	const nodeDependenciesProvider = new DepNodeProvider(vscode.workspace.rootPath);
+	vscode.window.registerTreeDataProvider('nodeDependencies', nodeDependenciesProvider);
 	context.subscriptions.push(disposable);
 }
 exports.activate = activate;
